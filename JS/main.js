b777f8de7ff6723a5352056c5753732c3bbcf7e8
smail1991 <zoubiresmail853@gmail.com>
2023-10-14 02:42:02 +0200

    Project-FrontEnd

---
+++ JS/main.js
@@ -0,0 +1,18 @@
+// Get references to the elements in the About Us section
+const expandButton = document.getElementById('expand-button');
+const additionalInfo = document.getElementById('additional-info');
+
+// Add an event listener to the button
+expandButton.addEventListener('click', () => {
+  // Toggle the visibility of the additional information
+  additionalInfo.classList.toggle('show');
+});
+
+
+
+
+
+
+   
+
+    
